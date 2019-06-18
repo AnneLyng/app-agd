8bc79845627c1fb3e1377ca2097db653b381e7fb
AnneLyng <als@sund.ku.dk>
2019-06-18 14:12:18 +0200

    prototype

--- server.R
+++ server.R
@@ -1,1 +1,18 @@
+## Purpose of script: (server.R) Shiny app (server-side) for AGD 
+##
+## Author: Anne Lyngholm Soerensen
+##
+## Date Created: 2019-06-18
+##
+## Copyright (c) Anne Lyngholm Soerensen, 2019
+## Email: lynganne@gmail.com
+
+server <- function(input, output, session){
+### the server function contains all output created for the app
+
+    observeEvent(input$tekstKnap, {
+        output$tekst <- renderText({"Hej Jørgen"})
+    })
+
+}
 
--- ui.R
+++ ui.R
@@ -1,1 +1,16 @@
+## Purpose of script: (ui.R) Shiny app User Interface (UI) for AGD
+##
+## Author: Anne Lyngholm Soerensen
+##
+## Date Created: 2019-06-18
+##
+## Copyright (c) Anne Lyngholm Soerensen, 2019
+## Email: lynganne@gmail.com
 
+library(shiny)
+
+ui <- fluidPage(
+    titlePanel("Prototype på AGD app"),
+    actionButton("tekstKnap", "Tryk her"),
+    verbatimTextOutput("tekst")
+    )
